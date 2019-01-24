035e2a0e50d759ab58d6de643d4b1327227f2dc3
Zavarzinx <nazgaard@gmail.com>
2019-01-24 15:55:52 +0400

    Initial commit

---
+++ NoteUp-api/src/main/java/project/note/NoteUpapi/NoteUpApiApplication.java
@@ -0,0 +1,14 @@
+package project.note.NoteUpapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NoteUpApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NoteUpApiApplication.class, args);
+	}
+
+}
+
---
+++ NoteUp-api/src/test/java/project/note/NoteUpapi/NoteUpApiApplicationTests.java
@@ -0,0 +1,17 @@
+package project.note.NoteUpapi;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class NoteUpApiApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
