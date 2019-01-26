652b134659e268c847636a09a133abd7597c5f3a
Zavarzinx <nazgaard@gmail.com>
2019-01-26 18:56:48 +0400

    Added basic routing

--- NoteUp-ng-app/src/app/app.module.ts
+++ NoteUp-ng-app/src/app/app.module.ts
@@ -3,14 +3,42 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavigationComponent } from './navigation/navigation.component';
+import { FeedbackComponent } from './feedback/feedback.component';
+import { NotesComponent } from './notes/notes.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import {Router, RouterModule, Routes} from "@angular/router";
 
+
+const appRoutes :Routes = [
+  {
+    path:'notes',
+    component:NotesComponent
+  },
+  {
+    path:'feedback',
+    component:FeedbackComponent
+  },
+  {
+    path:'',
+    component:NotesComponent,
+    pathMatch:'full'
+  },
+  {
+    path:'**',
+    component:NotFoundComponent
+  }
+];
 @NgModule({
   declarations: [
     AppComponent,
-    NavigationComponent
+    NavigationComponent,
+    FeedbackComponent,
+    NotesComponent,
+    NotFoundComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    RouterModule.forRoot(appRoutes, {enableTracing:true})
   ],
   providers: [],
   bootstrap: [AppComponent]
