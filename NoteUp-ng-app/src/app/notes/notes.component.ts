de4c56baccd694e94539a1dfd8033c016aa10f66
Zavarzinx <nazgaard@gmail.com>
2019-01-29 14:25:10 +0400

    added main service functions

--- NoteUp-ng-app/src/app/notes/notes.component.ts
+++ NoteUp-ng-app/src/app/notes/notes.component.ts
@@ -2,6 +2,8 @@ import { Component, OnInit } from '@angular/core';
 import {HttpClient} from "@angular/common/http";
 import {Notebook} from "./model/notebook";
 import {ApiService} from "../shared/api.service";
+import {Observable} from "rxjs";
+import {Note} from "./model/note";
 
 @Component({
   selector: 'app-notes',
@@ -10,11 +12,14 @@ import {ApiService} from "../shared/api.service";
 })
 export class NotesComponent implements OnInit {
   notebooks: Notebook[] = [];
+  notes:Note[];
+  selectedNotebook: Notebook;
 
   constructor(private apiService: ApiService) { }
 
   ngOnInit() {
     this.getAllNotebooks();
+    this.getAllNotes();
   }
 
   public getAllNotebooks(){
@@ -28,4 +33,109 @@ export class NotesComponent implements OnInit {
     );
   }
 
+  getAllNotes(){
+    this.apiService.getAllNotes().subscribe(
+      res => {
+        this.notes = res;
+      },
+      err=>{alert("Error has occured while getting notes")}
+    )
+  }
+
+  createNotebook() {
+    let newNotebook:Notebook = {
+      name:'New notebook',
+      id: null,
+      nbNotes:0
+    }
+    this.apiService.postNotebook(newNotebook).subscribe(
+      res => {
+        newNotebook.id = res.id;
+        this.notebooks.push(newNotebook);
+      },
+      err =>{
+        alert("An error has occurred while saving the notebook");
+      }
+
+    );
+  }
+
+  updateNotebook(updatedNotebook: Notebook) {
+    this.apiService.postNotebook(updatedNotebook).subscribe(
+      res => {
+
+      },
+      err =>{
+        alert("An error has occurred while saving the notebook");
+      }
+    );
+  }
+
+  deleteNotebook(notebook: Notebook) {
+    if(confirm("Are you sure you want to delete notebook?")){
+      this.apiService.deleteNotebook(notebook.id).subscribe(
+        res => {
+          let indexOfNotebook = this.notebooks.indexOf(notebook);
+          this.notebooks.splice(indexOfNotebook, 1);
+        },
+        err => {
+          alert("Couldnt delete notebook");
+        }
+      )
+    }
+  }
+
+  deleteNote(note: Note) {
+    if(confirm("Are you sure u want to delete this note?")){
+      this.apiService.deleteNote(note.id).subscribe(
+        res =>{
+          let indexOfNote = this.notes.indexOf(note);
+          this.notes.splice(indexOfNote, 1);
+        },
+        err =>{alert("An error has occurred while deleting")}
+      );
+    }
+  }
+
+  createNote(notebookId:string) {
+      let newNote:Note = {
+        id:null,
+        title:"New Note",
+        text:"Write text here",
+        notebookId: notebookId,
+        lastModifiedOn:null
+      };
+
+      this.apiService.saveNote(newNote).subscribe(
+        res =>{
+          newNote.id = res.id;
+          this.notes.push(newNote);
+        },
+        err => {alert("An error has occurrd while sending the note")}
+      )
+  }
+
+  selectNotebook(notebook: Notebook) {
+    this.selectedNotebook = notebook;
+    this.apiService.getNotesByNotebook(notebook.id).subscribe(
+      res=> {
+        this.notes = res;
+      },
+      err =>{alert("An error has occurred while downloading the notes;")}
+    );
+  }
+
+  updateNote(updatedNote: Note) {
+    this.apiService.saveNote(updatedNote).subscribe(
+      res =>{
+      },
+      err => {alert("An error has occurrd while updating the note")}
+    )
+    
+  }
+
+  selectAllNotes() {
+    this.selectedNotebook = null;
+    this.getAllNotes();
+  }
 }
--- NoteUp-ng-app/src/app/shared/api.service.ts
+++ NoteUp-ng-app/src/app/shared/api.service.ts
@@ -3,14 +3,21 @@ import {HttpClient} from "@angular/common/http";
 import {Observable} from "rxjs";
 import {Notebook} from "../notes/model/notebook";
 import {FeedbackViewModel} from "../feedback/feedback.component";
+import {Note} from "../notes/model/note";
 
 @Injectable({
   providedIn: 'root'
 })
 export class ApiService {
   private BASE_URL = "http://localhost:8080/api";
-  private ALL_NOTEBOOKS_URL = `${this.BASE_URL}\\notebooks\\all`;
-  private SEND_FEEDBACK_URL = `${this.BASE_URL}\\feedback`;
+  private ALL_NOTEBOOKS_URL = `${this.BASE_URL}/notebooks/all`;
+  private SEND_FEEDBACK_URL = `${this.BASE_URL}/feedback`;
+  private SAVE_UPDATE_NOTEBOOK_URL = `${this.BASE_URL}/notebooks`;
+  private DELETE_NOTEBOOK_URL = `${this.BASE_URL}/notebooks/`;
+  private ALL_NOTES_URL = `${this.BASE_URL}/notes/all`;
+  private NOTES_BY_NOTEBOOK_URL = `${this.BASE_URL}/notes/byNotebook/`;
+  private SAVE_UPDATE_NOTE_URL = `${this.BASE_URL}/notes`;
+  private DELETE_NOTE_URL = `${this.BASE_URL}/notes/`;
 
   constructor(private http: HttpClient) {
 
@@ -20,7 +27,30 @@ export class ApiService {
     return this.http.get<Notebook[]>(this.ALL_NOTEBOOKS_URL);
   }
 
+  getAllNotes() :Observable<Note[]>{
+    return this.http.get<Note[]>(this.ALL_NOTES_URL);
+  }
+
+  getNotesByNotebook(notebookId: string): Observable<Note[]> {
+    return this.http.get<Note[]>(this.NOTES_BY_NOTEBOOK_URL + notebookId);
+  }
+
   postFeedback(feedback : FeedbackViewModel) :Observable<any>{
     return this.http.post(this.SEND_FEEDBACK_URL, feedback);
+  }
+
+  postNotebook(notebook: Notebook):Observable<Notebook>{
+    return this.http.post<Notebook>(this.SAVE_UPDATE_NOTEBOOK_URL, notebook);
+  }
+
+  deleteNotebook(id:string):Observable<any>{
+    return this.http.delete(this.DELETE_NOTEBOOK_URL + id);
+  }
+  saveNote(note: Note):Observable<Note>{
+    return this.http.post<Note>(this.SAVE_UPDATE_NOTE_URL, note);
+  }
+
+  deleteNote(noteId:string):Observable<any>{
+    return this.http.delete(this.DELETE_NOTE_URL + noteId);
   }
 }
